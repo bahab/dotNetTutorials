28eafdc27e7dbdd17e5030586de17af6ff6e540a
Adam Carter <carteradamscott@gmail.com>
2013-01-24 16:44:35 +1100

    uyfv

---
+++ Test/Test/ImageTest.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Test
+{
+    partial class ImageTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainLayoutPane = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // mainLayoutPane
+            // 
+            this.mainLayoutPane.ColumnCount = 2;
+            this.mainLayoutPane.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayoutPane.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayoutPane.Location = new System.Drawing.Point(12, 3);
+            this.mainLayoutPane.Name = "mainLayoutPane";
+            this.mainLayoutPane.RowCount = 2;
+            this.mainLayoutPane.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayoutPane.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayoutPane.Size = new System.Drawing.Size(1030, 653);
+            this.mainLayoutPane.TabIndex = 0;
+            this.mainLayoutPane.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // ImageTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1069, 668);
+            this.Controls.Add(this.mainLayoutPane);
+            this.Name = "ImageTest";
+            this.Text = "ImageTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainLayoutPane;
+    }
+}
+
